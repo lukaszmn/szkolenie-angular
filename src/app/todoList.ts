2619747cf4d257c22115aa6701380bda6a665631
Lukasz Nojek <9399633+lukaszmn@users.noreply.github.com>
2023-04-19 18:08:57 +0200

    Bootstrap, tablica 2 kolumny

---
+++ src/app/todoList.ts
@@ -0,0 +1,15 @@
+export class ListaZadan {
+
+  constructor(nazwa: string) {
+    this.nazwa = nazwa;
+  }
+
+  nazwa: string;
+
+  lista = [
+    'Kupić telefon',
+    'Wymienić opony',
+    'Wynieść śmieci'
+  ];
+
+}
